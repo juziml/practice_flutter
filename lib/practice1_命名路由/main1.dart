aa2de861f5072c846e1b64fe987463566dedc48e
zhusw <sink50@foxmail.com>
2021-05-11 23:22:12 +0800

    -注册路由 -路由跳转，获取返回值

--- lib/practice1_命名路由/main1.dart
+++ lib/practice1_命名路由/main1.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
         canvasColor: Colors.blueAccent,
       ),
       initialRoute: "/",//
+      //注册路由
       routes: {
        "/":(context){
           return HomePage();
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-//这是页的纬度
+//这是页的维度
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
