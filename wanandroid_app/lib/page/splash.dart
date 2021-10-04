3d2d3d46302f5797a6540c0c030a926a9bdb7b64
sink5 <harkben50@gmail.com>
2021-10-04 15:45:03 +0800

    启动页面

---
+++ wanandroid_app/lib/page/splash.dart
@@ -0,0 +1,43 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:wanandroid_app/c/color_list.dart';
+
+class SplashPage extends StatelessWidget{
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: MColors.primaryColorDark,
+      body:ConstrainedBox(
+        constraints: const BoxConstraints(
+          maxWidth: double.infinity,
+          maxHeight: double.infinity
+        ),
+        child: Center(
+          widthFactor: double.infinity,
+          heightFactor: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:  [
+              // Image.asset("images/icon_flutter.png",width: 100.0),
+              Image(image: AssetImage("images/icon_flutter.png"),width: 80.0),
+              Padding(padding: EdgeInsets.all(20)),
+              const Text(
+                "By WanAndroid open API",
+                style: TextStyle(
+                  fontStyle: FontStyle.normal,
+                  color: Colors.white,
+                  fontSize: 40,
+                ),),
+            ],
+          ),
+        )
+
+      ),
+
+    );
+  }
+
+}
