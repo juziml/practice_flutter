07abeb4e89d85240b3b8c6ae959fd644ddc61c4b
sink5 <harkben50@gmail.com>
2021-10-14 21:49:56 +0800

    dev:添加分割线

--- wanandroid_app/lib/routes/feed/feed_page2.dart
+++ wanandroid_app/lib/routes/feed/feed_page2.dart
@@ -1,12 +1,8 @@
-import 'dart:ui';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:wanandroid_app/color/color_list.dart';
 import 'package:wanandroid_app/wiget/banner_widget.dart';
 import 'package:wanandroid_app/model/banner_entity.dart';
-
-/// 模拟listView 上拉加载更多
 
 class FeedPage extends StatefulWidget {
   const FeedPage({Key? key}) : super(key: key);
@@ -20,8 +16,6 @@ class _FeedPageState extends State<FeedPage>{
   List<BannerEntity> banners = [BannerEntity("http://tva1.sinaimg.cn/large/005Jb6GUgy1gvamn2yey2j609t05ggms02.jpg")];
   @override
   Widget build(BuildContext context) {
-    Widget divider1 = Divider(color: Colors.pink);
-    Widget divider2 = Divider(color: Colors.purple);
     return Scaffold(
         backgroundColor: MColors.pageBackground,
         body:Container(
@@ -33,17 +27,12 @@ class _FeedPageState extends State<FeedPage>{
                     child:  BannerWidget(banners)),
                 Expanded(
                     flex: 1,
-                    child:ListView.separated(
+                    child: ListView.builder(
                     itemCount: 100,
+                    itemExtent: 50,
                     itemBuilder: (BuildContext context, int index) {
                       return ListTile(title: Text("$index"));
-                    },
-                      separatorBuilder: (context,index){
-                        return index%2==0?divider1:divider2;
-                      },
-
-                    )
-                )
+                    }))
 
               ],
             )));
